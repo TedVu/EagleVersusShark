3e47609ab801d941a8167a9dd89f2a1c4046a9f8
Kevin Vu <kevinvu184@gmail.com>
2020-04-07 19:20:50 +1000

    Refactor

--- src/view/operationview/ModePanel.java
+++ src/view/operationview/ModePanel.java
@@ -1,8 +1,7 @@
 package view.operationview;
 
-import java.awt.BorderLayout;
-
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
@@ -13,18 +12,23 @@ public class ModePanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 4010321472922982018L;
-	private JComboBox<String> modesMenu;
+	private JComboBox<String> modeBox;
 	private JButton undoBtn;
-	
-	public ModePanel() {	
+
+	public ModePanel() {
 		String[] modes = { "Please select mode", "Normal Mode", "Sky Mode", "Protection Mode" };
-		modesMenu = new JComboBox<String>(modes);
+		modeBox = new JComboBox<String>(modes);
 		undoBtn = new JButton("Undo Move");
-		
+
 		setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Mode Panel"));
-		setLayout(new BorderLayout());
-		
-		add(modesMenu, BorderLayout.NORTH);
-		add(undoBtn, BorderLayout.SOUTH);
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		JPanel compoBoxPannel = new JPanel();
+		compoBoxPannel.add(modeBox);
+		add(compoBoxPannel);
+
+		JPanel undoBtnPanel = new JPanel();
+		undoBtnPanel.add(undoBtn);
+		add(undoBtnPanel);
 	}
 }
