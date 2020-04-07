136ad3b322ca87196789d4d1a08b3d6d8006e120
Kevin Vu <kevinvu184@gmail.com>
2020-04-07 13:52:18 +1000

    Delete unsued tag

--- src/asset/PieceType.java
+++ src/asset/PieceType.java
@@ -2,171 +2,118 @@ package asset;
 
 public enum PieceType {
 	ATTACKINGEAGLE {
-
 		@Override
 		public String getFileName() {
-			// TODO Auto-generated method stub
 			return "/asset/AttackingEagle.png";
 		}
-
 		@Override
 		public int getInitialPosX() {
-			// TODO Auto-generated method stub
 			return 3;
 		}
-
 		@Override
 		public int getInitialPosY() {
-			// TODO Auto-generated method stub
 			return 0;
 		}
-
 		@Override
 		public String toString() {
-			// TODO Auto-generated method stub
 			return "AttackingEagle";
 		}
-
 	},
 
 	LEADERSHIPEAGLE {
-
 		@Override
 		public String getFileName() {
-			// TODO Auto-generated method stub
 			return "/asset/LeadershipEagle.png";
 		}
-
 		@Override
 		public int getInitialPosX() {
-			// TODO Auto-generated method stub
 			return 4;
 		}
-
 		@Override
 		public int getInitialPosY() {
-			// TODO Auto-generated method stub
 			return 1;
 		}
-
 		@Override
 		public String toString() {
-			// TODO Auto-generated method stub
 			return "LeadershipEagle";
 		}
-
 	},
-
+	
 	VISIONARYEAGLE {
-
 		@Override
 		public String getFileName() {
-			// TODO Auto-generated method stub
 			return "/asset/VisionaryEagle.png";
 		}
-
 		@Override
 		public int getInitialPosX() {
-			// TODO Auto-generated method stub
 			return 5;
 		}
-
 		@Override
 		public int getInitialPosY() {
-			// TODO Auto-generated method stub
 			return 0;
 		}
-
 		@Override
 		public String toString() {
-			// TODO Auto-generated method stub
 			return "VisionaryEagle";
 		}
-
 	},
 
 	AGGRESSIVESHARK {
-
 		@Override
 		public String getFileName() {
-			// TODO Auto-generated method stub
 			return "/asset/AggressiveShark.png";
 		}
-
 		@Override
 		public int getInitialPosX() {
-			// TODO Auto-generated method stub
 			return 3;
 		}
-
 		@Override
 		public int getInitialPosY() {
-			// TODO Auto-generated method stub
 			return 8;
 		}
-
 		@Override
 		public String toString() {
-			// TODO Auto-generated method stub
 			return "AggressiveShark";
 		}
 
 	},
 
 	DEFENSIVESHARK {
-
 		@Override
 		public String getFileName() {
-			// TODO Auto-generated method stub
 			return "/asset/DefensiveShark.png";
 		}
-
 		@Override
 		public int getInitialPosX() {
-			// TODO Auto-generated method stub
 			return 4;
 		}
-
 		@Override
 		public int getInitialPosY() {
-			// TODO Auto-generated method stub
 			return 7;
 		}
-
 		@Override
 		public String toString() {
-			// TODO Auto-generated method stub
 			return "DefensiveShark";
 		}
-
 	},
 
 	HEALINGSHARK {
-
 		@Override
 		public String getFileName() {
-			// TODO Auto-generated method stub
 			return "/asset/HealingShark.png";
 		}
-
 		@Override
 		public int getInitialPosX() {
-			// TODO Auto-generated method stub
 			return 5;
 		}
-
 		@Override
 		public int getInitialPosY() {
-			// TODO Auto-generated method stub
 			return 8;
 		}
-
 		@Override
 		public String toString() {
-			// TODO Auto-generated method stub
 			return "HealingShark";
 		}
-
 	};
 
 	private static String eagleTeamName = "eagleplayer";
@@ -188,5 +135,4 @@ public enum PieceType {
 	public abstract int getInitialPosY();
 
 	public abstract String toString();
-
 }
