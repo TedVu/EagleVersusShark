1f86369da929d562594ea7b4f486aea3bc572b84
sefira karina <sefirakh@gmail.com>
2020-04-16 10:41:31 +1000

    fix commenting for engine & player

--- src/model/player/PlayerImpl.java
+++ src/model/player/PlayerImpl.java
@@ -4,6 +4,10 @@ import model.enumtype.TeamType;
 
 /**
  * @author sefira
+ * 
+ * class to control the activation of each team
+ * in the future this class will also control whether
+ * the player can undo
  *
  */
 public class PlayerImpl implements Player {
@@ -11,9 +15,7 @@ public class PlayerImpl implements Player {
 	private boolean isActive;
 	private TeamType playerType;
 
-	/**
-	 * @param playerType
-	 */
+	
 	public PlayerImpl(TeamType playerType) {
 		this.playerType = playerType;
 	}
