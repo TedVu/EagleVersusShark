cd8cf8889801d96768d9c1b205c8c0e7948bb76c
ferics <schanboth@hotmail.com>
2020-05-02 22:53:21 +1000

    Refactored DefensiveShark's ability code

--- src/model/piece/DefensiveShark.java
+++ src/model/piece/DefensiveShark.java
@@ -17,7 +17,7 @@ import model.piece.movement.DiagonalMove;
  */
 public class DefensiveShark extends AbstractPiece {
 
-	final int DISTANCE = 1;
+	final int NEIGHBOURING_DISTANCE = 1;
 	private final EngineInterface engine;
 
 	public DefensiveShark(int x, int y, EngineInterface engine) {
@@ -42,33 +42,28 @@ public class DefensiveShark extends AbstractPiece {
 	public void useAbility(PieceAbility pieceAbility, PieceInterface piece, PieceInterface affectedPiece) {
 		if (pieceAbility.equals(PieceAbility.PROTECT)) {
 			defend(affectedPiece);
-		} else if (pieceAbility.equals(PieceAbility.QUICKMOVE)) {
-			// QUICKMOVE = a normal move piece action see what needs to be updated when
-			// moving a piece for more information, no affectedPiece required
-
 		} else {
-
 			throw new IllegalArgumentException("Invalid ability");
 		}
 	}
 
 	// Overload method for second ability
-	public void useAbility(PieceAbility pieceAbility, PieceInterface piece, Cell surroundingCell){
-		if(pieceAbility.equals(PieceAbility.QUICKMOVE)){
-			quickMove(piece,surroundingCell);
-		} else {
-			throw new IllegalArgumentException("Invalid ability");
-		}
-	}
-
-	private void quickMove(PieceInterface piece, Cell surroundingCell){
-		Set<Cell> neighbourCells = abilityCells();
-		if(neighbourCells.contains(surroundingCell)){
-			piece.movePiece(surroundingCell.getX(),surroundingCell.getY());
-		} else {
-			throw new IllegalArgumentException("Invalid move");
-		}
-	}
+//	public void useAbility(PieceAbility pieceAbility, PieceInterface piece, Cell surroundingCell){
+//		if(pieceAbility.equals(PieceAbility.QUICKMOVE)){
+//			quickMove(piece,surroundingCell);
+//		} else {
+//			throw new IllegalArgumentException("Invalid ability");
+//		}
+//	}
+//
+//	private void quickMove(PieceInterface piece, Cell surroundingCell){
+//		Set<Cell> neighbourCells = abilityCells();
+//		if(neighbourCells.contains(surroundingCell)){
+//			piece.movePiece(surroundingCell.getX(),surroundingCell.getY());
+//		} else {
+//			throw new IllegalArgumentException("Invalid move");
+//		}
+//	}
 
 	private void defend(PieceInterface targetPiece) {
 		try {
@@ -87,6 +82,7 @@ public class DefensiveShark extends AbstractPiece {
 	@Ensures("abilityCells() != null")
 	public Set<Cell> abilityCells() {
 		Set<Cell> neighbourCells = new HashSet<>();
+		Set<Cell> surroundingEightCells = new HashSet<>();
 		List<PieceInterface> activeSharks = engine.pieceOperator().getActiveSharks();
 
 		// Return all the neighbouring cells around other sharks
@@ -96,20 +92,17 @@ public class DefensiveShark extends AbstractPiece {
 				int y = shark.getPosition().get("y");
 
 				// All the neighbour cells around a shark
-				// Undergoing further code review and refactoring..
-				LinkedList<Cell> surroundingEightCell = new LinkedList<>();
-				surroundingEightCell.add(engine.getBoard().getCell(x - 1, y - 1));
-				surroundingEightCell.add(engine.getBoard().getCell(x, y - 1));
-				surroundingEightCell.add(engine.getBoard().getCell(x + 1, y - 1));
-				surroundingEightCell.add(engine.getBoard().getCell(x + 1, y));
-				surroundingEightCell.add(engine.getBoard().getCell(x + 1, y + 1));
-				surroundingEightCell.add(engine.getBoard().getCell(x, y + 1));
-				surroundingEightCell.add(engine.getBoard().getCell(x - 1, y + 1));
-				surroundingEightCell.add(engine.getBoard().getCell(x-1,y));
+				surroundingEightCells.addAll(new DiagonalMove().getValidMove(shark,NEIGHBOURING_DISTANCE));
 
-				// Add the cell to the return list if it has not been occupied by any other piece AND is within boardSize
-				for (Cell possibleCell : surroundingEightCell){
+				/*
+				 * Add the cell to the return list if the following are true:
+				 * 		Cell is not occupied
+				 * 		Cell is not the master cell
+				 * 		Cell is within the board
+				 */
+				for (Cell possibleCell : surroundingEightCells){
 					if(!possibleCell.getOccupied() &&
+					   !possibleCell.getIsMasterCell() &&
 						possibleCell.getY()<engine.getBoard().getSize()-1 &&
 						possibleCell.getY()>=0 &&
 						possibleCell.getX()>=0 &&
