4ad4b3a314d417af969fc9a602a7b553f715d2f1
sefira karina <sefirakh@gmail.com>
2020-05-15 23:13:52 +1000

    remove unused methods in engine

--- src/model/engine/EngineImpl.java
+++ src/model/engine/EngineImpl.java
@@ -133,177 +133,18 @@ public class EngineImpl implements Engine, Serializable {
 		return gamePiece;
 	}
 
-//	/**
-//	 * stop the timer of the game
-//	 */
-//	@Override
-//	@Requires({ "eaglePlayer.getActive() == true || eaglePlayer.getActive() == false" })
-//	public void cancelTimer() {
-//		TeamType currentPlayer = eaglePlayer.getActive() ? TeamType.SHARK : TeamType.EAGLE;
-//		geCallback.nextMove(currentPlayer);
-//		if (gameTimer != null) {
-//			gameTimer.cancel();
-//		}
-//	}
 
 	@Override
 	public GameBoard gameBoard() {
 		return board;
 	}
 
-//	/**
-//	 * get the current player turn
-//	 * 
-//	 * @return the active team
-//	 */
-//	@Override
-//	public Player getCurrentActivePlayer() {
-//		if (eaglePlayer.getActive())
-//			return eaglePlayer;
-//		else
-//			return sharkPlayer;
-//
-//	}
-
-//	@Override
-//	public GameEngineCallbackInterface getGameEngineCallback() {
-//		return geCallback;
-//	}
-
-//	/*
-//	 * return the initial active player, call this at the beginning of the program
-//	 * 
-//	 * @return (eaglePlayer || sharkPlayer)
-//	 */
-//	@Requires({ "eaglePlayer!= null", "sharkPlayer != null" })
-//	@Override
-//	public Player getInitialPlayerActivePlayer() {
-//		gameRunning = true;
-//		Player activePlayer;
-//		Random rand = new Random();
-//		int randomValue = 0;
-//		if (eaglePlayer.getActive() == true) {
-//			randomValue = 0;
-//		} else if (sharkPlayer.getActive() == true) {
-//			randomValue = 1;
-//		} else {
-//			randomValue = rand.nextInt() % 2;
-//		}
-//		if (randomValue == 0) {
-//			this.eaglePlayer.setActive(true);
-//			this.sharkPlayer.setActive(false);
-//			activePlayer = eaglePlayer;
-//		} else {
-//			this.eaglePlayer.setActive(false);
-//			this.sharkPlayer.setActive(true);
-//			activePlayer = sharkPlayer;
-//		}
-//		return activePlayer;
-//	}
-
-//	/**
-//	 * set the turn to the specified team in parameter
-//	 * 
-//	 * @param playerType
-//	 *            - the player to be activated
-//	 * @param turnOnTimer
-//	 *            - whether to begin countdown or not
-//	 */
-//	@Override
-//	@Requires({ "playerType != null", "turnOnTimer == true || turnOnTimer == false" })
-//	public void setActivePlayer(TeamType playerType, boolean turnOnTimer) {
-//
-//		TeamType nextPlayer;
-//		turn++;
-//		round = turn / 2;
-//
-//		// System.out.println("turn: " + turn);
-//		System.out.println("round: " + round);
-//
-//		if (playerType == TeamType.EAGLE) {
-//			this.eaglePlayer.setActive(true);
-//			this.sharkPlayer.setActive(false);
-//			nextPlayer = TeamType.SHARK;
-//		} else if (playerType == TeamType.SHARK) {
-//			this.eaglePlayer.setActive(false);
-//			this.sharkPlayer.setActive(true);
-//			nextPlayer = TeamType.EAGLE;
-//		} else {
-//			throw new IllegalArgumentException("invalid player type, must be eagle or shark");
-//		}
-//		if (turnOnTimer) {
-//			setActivePlayerTimer(nextPlayer);
-//		}
-//	}
-
-//	/**
-//	 * turn on the timer and loop call setActivePlayer to change every interval
-//	 * 
-//	 * @param playerType
-//	 *            - the player to be activated next
-//	 */
-//	@Override
-//	@Requires({ "playerType != null" })
-//	public void setActivePlayerTimer(TeamType playerType) {
-//		gameTimer = new java.util.Timer();
-//		gameTimer.schedule(new java.util.TimerTask() {
-//			@Override
-//			public void run() {
-//				setActivePlayer(playerType, true);
-//				TeamType currentPlayer = eaglePlayer.getActive() ? TeamType.EAGLE : TeamType.SHARK;
-//				geCallback.timerNextMove(playerType, currentPlayer);
-//			}
-//		}, 10000);
-//	}
 
 	@Override
 	public GamePiece pieceOperator() {
 		return gamePiece;
 	}
 
-//	@Override
-//	public void cancelTimerPauseGame() {
-//		gameRunning = false;
-//		gameTimer.cancel();
-//	}
-
-//	@Override
-//	public void incrementUndo(TeamType teamType) {
-//
-//		Player player = playerType(teamType);
-//
-//		player.undoCounter(round);
-//	}
-
-//	@Override
-//	public boolean ableToUndo(TeamType teamType) {
-//
-//		Player player = playerType(teamType);
-//
-//		return player.ableToUndo(round);
-//	}
-//
-//	private Player playerType(TeamType teamType) {
-//
-//		if (teamType.equals(TeamType.EAGLE)) {
-//			return eaglePlayer;
-//		} else if (teamType.equals(TeamType.SHARK)) {
-//			return sharkPlayer;
-//		} else {
-//			throw new IllegalArgumentException("Invalid team type");
-//		}
-//
-//	}
-
-//	@Override
-//	public boolean getGameCurrentlyRunning() {
-//		return gameRunning;
-//	}
-//
-//	@Override
-//	public void setResumeGame() {
-//		gameRunning = true;
-//	}
 
 	@Override
 	public int getTotalNumPiece() {
